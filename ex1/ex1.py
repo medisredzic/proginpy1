c096376a9786fbd00031de98c5a02aec7bccce83
Medis Redzic <58595200+medisredzic@users.noreply.github.com>
2022-01-11 01:12:40 +0100

    ex18

---
+++ ex1/ex1.py
@@ -0,0 +1,10 @@
+"""
+Author: Medis Redzic
+Matr.Nr.: K11934616
+Exercise: Exercise 1
+"""
+
+a = 3 + 7
+print(a)
+print("I read the instructions for submitting homework.")
+print("I completed exercise 1!")
