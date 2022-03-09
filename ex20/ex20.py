66958f13a1fcb66b04492c3ffae929b1fc5f4d86
Medis Redzic <58595200+medisredzic@users.noreply.github.com>
2022-03-09 21:39:31 +0100

    ex20

---
+++ ex20/ex20.py
@@ -0,0 +1,196 @@
+"""
+Author: Medis Redzic
+Matr.Nr.: K11934616
+Exercise: Exercise 20
+"""
+import numpy
+
+"""
+Author -- Michael Widrich, Andreas Schörgenhumer
+Contact -- schoergenhumer@ml.jku.at
+updated by: Van Quoc Phuong Huynh, 11.2021
+
+###############################################################################
+
+The following copyright statement applies to all code within this file.
+
+Copyright statement:
+This  material,  no  matter  whether  in  printed  or  electronic  form,
+may  be  used  for personal  and non-commercial educational use only.
+Any reproduction of this manuscript, no matter whether as a whole or in parts,
+no matter whether in printed or in electronic form, requires explicit prior
+acceptance of the authors.
+
+###############################################################################
+
+Template file for solution of assignment 3, exercises 18, 19, 20, 21. Contains
+backbone for GameOfLife class and example usage of GameOfLife at the end of the
+file. Make sure you only changed the methods you are supposed to change in
+the file you hand in as your solution.
+"""
+
+# Add your import statements here
+
+# Do not modify these import statements
+import os
+
+import numpy as np
+import tqdm
+from matplotlib import pyplot as plt
+
+
+class GameOfLife:
+
+    # Here you should modify the __init__ method (ex18, ex21)
+    def __init__(self, config_file: str, state_output_file: str, plot_output_dir: str, video_output_file: str):
+        self.iteration_count, self.dead_symbol, self.live_symbol, self.init_state, self.scope = self.read_config_file(config_file)
+        self.state_output_file = state_output_file
+        self.plot_output_dir = plot_output_dir
+        self.video_output_file = video_output_file
+        self.state = self.init_state
+        self.current_iteration = 0
+
+        os.makedirs(state_output_file, exist_ok=True)
+        os.makedirs(plot_output_dir, exist_ok=True)
+        os.makedirs(video_output_file, exist_ok=True)
+
+    # Here you should modify the __write_state__ method (ex19)
+    def __write_state__(self, live_symbol: str = 'None', dead_symbol: str = 'None'):
+        dead_sy = self.dead_symbol
+        live_sy = self.live_symbol
+
+        if live_symbol:
+            if len(live_symbol) == 1:
+                live_sy = live_symbol
+
+        if dead_symbol:
+            if len(dead_symbol) == 1:
+                dead_sy = dead_symbol
+
+        format_arr = np.where(self.state == 0, dead_sy, live_sy)
+
+        file = open(self.state_output_file, 'w')
+        for n in format_arr:
+            file.write(''.join(n) + '\n')
+        file.write('\n')
+        file.close()
+
+    # Here you should modify the make_video method (ex20)
+    def make_video(self):
+        os.makedirs(self.video_output_file, exist_ok=True)
+        if os.name == 'posix':
+            os.system(f'ffmpeg -r 10 -pattern_type glob -i {self.plot_output_dir}/*.png -c:v libx264 -vprofile baseline \
+                      -pix_fmt yuv420p -c:a aac -movflags faststart {self.video_output_file}.mp4')
+        if os.name == 'nt':
+            os.system(f'ffmpeg -r 10 -pattern_type sequence -i "{self.plot_output_dir}/state_%05d.png" \
+            -c:v libx264 -pix_fmt yuv420p -c:a aac -movflags faststart {self.video_output_file}.mp4')
+
+    @staticmethod
+    def read_config_file(config_file: str):
+        """Template version of read_config_file(). This is not a valid solution for ex16."""
+        init_state = np.array(
+            [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+        iteration_count = 10
+        dead_symbol = '-'
+        live_symbol = 'o'
+        scope = [1, 10, 10, 27]
+        return iteration_count, dead_symbol, live_symbol, init_state, scope
+
+    # Here you should modify the step method (ex21)
+    def step(self):
+        """Compute the next tick of the simulator and return current number of iteration.
+        Returns None if game is completed."""
+        self.current_iteration += 1
+        if self.current_iteration > self.iteration_count:
+            return None
+        if self.current_iteration == 1:
+            self.state = self.gen_next_state(self.state, self.scope)
+        else:
+            self.state = self.gen_next_state(self.state, None)
+        self.__write_state__()
+        self.__state_to_image__()
+        return self.current_iteration
+
+    def gen_next_state(self, state: np.ndarray, scope):
+        """Template version of gen_next_state(). This is not a valid solution for ex17."""
+        temp_state_1 = np.array(
+            [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+        temp_state_2 = np.array(
+            [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+        if np.all(self.state == temp_state_1):
+            new_state = temp_state_2
+        else:
+            new_state = temp_state_1
+        return new_state
+
+    def __state_to_image__(self):
+        """Save state to image file"""
+        image_file = os.path.join(self.plot_output_dir, f"state_{self.current_iteration:05}.png")
+        fig, ax = plt.subplots()
+        ax.imshow(np.asarray(self.state, dtype=np.uint8))
+        fig.tight_layout()
+        fig.savefig(image_file)
+        plt.close(fig)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    # Create a parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config_file', help='configuration file', type=str)
+    parser.add_argument('state_output_file', help='file to write state to', type=str)
+    parser.add_argument('plot_output_dir', help='directory to write plotted states to', type=str)
+    parser.add_argument('video_output_file', help='file to write video to', type=str)
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Create game instance
+    game = GameOfLife(config_file=args.config_file, state_output_file=args.state_output_file,
+                      plot_output_dir=args.plot_output_dir, video_output_file=args.video_output_file)
+
+    current_iteration = 0
+    with tqdm.tqdm() as progressbar:  # Show a progressbar
+        while current_iteration is not None:  # Continue until current iteration is None (=End of game)
+            current_iteration = game.step()
+            progressbar.update()
+
+    # Save video to file
+    game.make_video()
